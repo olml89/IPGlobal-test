fba6219b1b84ef8fa692b3be8f72d38ce1ef44dc
Oleguer Mas <oleguer.ml89@gmail.com>
2023-03-19 14:29:53 +0100

    Implement json restriction at the api level (api must consume and return the data in JSON format)

--- app/Common/Infrastructure/Laravel/Http/Kernel.php
+++ app/Common/Infrastructure/Laravel/Http/Kernel.php
@@ -42,6 +42,7 @@ class Kernel extends HttpKernel
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \olml89\IPGlobalTest\Common\Infrastructure\Laravel\Http\Middleware\EnforceJson::class,
         ],
     ];
 
---
+++ app/Common/Infrastructure/Laravel/Http/Middleware/EnforceJson.php
@@ -0,0 +1,37 @@
+<?php declare(strict_types=1);
+
+namespace olml89\IPGlobalTest\Common\Infrastructure\Laravel\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Symfony\Component\HttpKernel\Exception\NotAcceptableHttpException;
+use Symfony\Component\HttpKernel\Exception\UnsupportedMediaTypeHttpException;
+
+final class EnforceJson
+{
+    /**
+     * @throws NotAcceptableHttpException | UnsupportedMediaTypeHttpException
+     */
+    public function handle(Request $request, Closure $next, string ...$guards): JsonResponse
+    {
+        // Http Not Acceptable
+        if (!$request->wantsJson()) {
+            return new JsonResponse(
+                data: ['message' => 'This api only returns data in \'application/json\' format'],
+                status: 406,
+            );
+        }
+
+        // Http Unsupported Media Type
+        if (!$request->isJson()) {
+            return new JsonResponse(
+                data: ['message' => 'This api only consumes data in \'application/json\' format'],
+                status: 415,
+            );
+        }
+
+        return $next($request);
+    }
+}
