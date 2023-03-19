63a040122535cc4a3bae8a563a9ff9f5c409404d
Oleguer Mas <oleguer.ml89@gmail.com>
2023-03-19 17:50:57 +0100

    Install laravel mix and uninstall vite to work with webpack on the frontend assets

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>List of posts</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+    <!-- Styles -->
+    <link rel="stylesheet" href=""></head>
+</head>
+<body class="antialiased">
+
+<main>
+    <header>
+    </header>
+    <section>
+        <article>
+            Lorem ipsum sit amet
+        </article>
+        <article>
+            Lorem ipsum sit amet
+        </article>
+        <article>
+            Lorem ipsum sit amet
+        </article>
+        <article>
+            Lorem ipsum sit amet
+        </article>
+        <article>
+            Lorem ipsum sit amet
+        </article>
+    </section>
+    <footer>
+
+    </footer>
+</main>
+
+</body>
+</html>
